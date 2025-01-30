1f0cefcf28639494aece475db47d55eafb26fe15
Alcander122 <andresdavidayalacortes@gmail.com>
2025-01-30 17:41:53 -0500

    Se  actualiza la rama de master de developer

--- lib/core/data/services/ingresos_service.dart
+++ lib/core/data/services/ingresos_service.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:finances/core/data/models/egreso_model.dart';
 
 class IngresosService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
