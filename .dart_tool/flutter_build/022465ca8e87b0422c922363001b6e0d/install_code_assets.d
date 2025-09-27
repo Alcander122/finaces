706922d0b76b10cdfe7efb9a3a3ec43e69bbc1cc
andresdiaza <andresdia19@hotmail.com>
2025-09-27 10:07:28 -0500

    Se realiza configuracion inicial del proyecto

--- .dart_tool/flutter_build/022465ca8e87b0422c922363001b6e0d/install_code_assets.d
+++
@@ -1,1 +0,0 @@
- D:\\FlutterProjects\\finaces\\.dart_tool\\flutter_build\\022465ca8e87b0422c922363001b6e0d\\native_assets.json: 
--- .dart_tool/flutter_build/b2b0bf1289879790af83bdc69704b059/install_code_assets.d
+++
@@ -1,1 +0,0 @@
- D:\\FlutterProjects\\finaces\\.dart_tool\\flutter_build\\b2b0bf1289879790af83bdc69704b059\\native_assets.json: 
